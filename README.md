3bcd016503b04cce368fab5c6ebdb80b7541bb22
fakhraldeen alfallatah <fakhraldeen18@gmail.com>
2024-09-20 22:28:21 +0300

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+// to install packages 
+in command  npm install vite
+// to run the project
+npm run dev
---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import "./App.css"
+
+function App() {
+  return (
+    <div>
+      <h1 className=" underline text-3xl">Hello World !! </h1>
+    </div>
+  )
+}
+export default App
