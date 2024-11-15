351129660755e06731cf3e108e20a910e73c5661
fakhraldeen alfallatah <fakhraldeen18@gmail.com>
2024-11-16 02:32:10 +0300

    add validation to signUp page

--- src/Pages/SignUp.tsx
+++ src/Pages/SignUp.tsx
@@ -1,22 +1,28 @@
 import { Link, useNavigate } from "react-router-dom"
 import { ChangeEvent, FormEvent, useState } from "react"
-import { Button } from "@/components/ui/button"
+import { SubmitHandler, useForm } from "react-hook-form"
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
+import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 
 export default function SignUp() {
-    const navigate = useNavigate()
+  type SignUpSchemaType = z.infer<typeof SignUpSchema>
+
+  const navigate = useNavigate()
   const [user, setUser] = useState({
     name: "",
     password: "",
     email: "",
     phone: ""
   })
-  const handleSubmit = (e: FormEvent) => {
+  const handleSubmitOld = (e: FormEvent) => {
     e.preventDefault()
     console.log("user:", user)
-        navigate("/login")
+    navigate("/login")
   }
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
@@ -27,10 +33,41 @@ export default function SignUp() {
     })
   }
 
+  const SignUpSchema = z
+    .object({
+      name: z.string().min(3).max(30),
+      email: z.string().email(),
+      phone: z.string({
+        required_error: "required field",
+        invalid_type_error: "Phone is required"
+      }).min(10),
+      password: z
+        .string()
+        .min(8, { message: "Password is too short" })
+        .max(20, { message: "Password is too long" }),
+      confirmPassword: z.string()
+    })
+    .refine((data) => data.password === data.confirmPassword, {
+      message: "Passwords do not match",
+      path: ["confirmPassword"] // path of error
+    })
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<SignUpSchemaType>({ resolver: zodResolver(SignUpSchema) })
+  console.log(errors.email?.message)
+
+  //we should here call like API or something...
+  const onSubmit: SubmitHandler<SignUpSchemaType> = (data: any) => {
+    console.log(data)
+  }
+
   return (
     <div className=" flex flex-col justify-between">
       <></>
-      <form className="mr-8" onSubmit={handleSubmit}>
+      <form className="mr-8" onSubmit={handleSubmit(onSubmit)}>
         <Card className="mx-auto max-w-sm mt-10 lg:mt-20 ml-10 md:ml-auto rounded ">
           <CardHeader>
             <CardTitle className="text-xl">Sign Up</CardTitle>
@@ -41,49 +78,57 @@ export default function SignUp() {
               <div className="grid gap-2">
                 <Label htmlFor="name">name</Label>
                 <Input
-                  name="name"
                   id="name"
                   type="text"
                   placeholder="Max Robinson"
-                  required
-                  onChange={handleChange}
+                  {...register("name")}
                   className="rounded "
                 />
+                {errors.name && <span className="text-red-600">{errors.name.message}</span>}
               </div>
               <div className="grid gap-2">
                 <div className="grid gap-2">
                   <Label htmlFor="email">Email</Label>
                   <Input
-                    name="email"
                     id="email"
                     type="email"
                     placeholder="m@example.com"
-                    required
-                    onChange={handleChange}
+                    {...register("email")}
                     className="rounded "
                   />
+                  {errors.email && <span className="text-red-600">{errors.email.message}</span>}
                 </div>
                 <Label htmlFor="phone">Phone</Label>
                 <Input
-                  name="phone"
                   id="phone"
                   type="number"
                   placeholder="966501234567"
-                  required
-                  onChange={handleChange}
+                  {...register("phone")}
                   className="rounded "
                 />
+                {errors.phone && <span className="text-red-600">{errors.phone.message}</span>}
               </div>
               <div className="grid gap-2">
                 <Label htmlFor="password">Password</Label>
                 <Input
-                  name="password"
                   id="password"
                   type="password"
-                  required
-                  onChange={handleChange}
+                  {...register("password")}
                   className="rounded "
                 />
+                {errors.password && <span className="text-red-600">{errors.password.message}</span>}
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="password">confirmPassword</Label>
+                <Input
+                  id="confirmPassword"
+                  type="password"
+                  {...register("confirmPassword")}
+                  className="rounded "
+                />
+                {errors.confirmPassword && (
+                  <span className="text-red-600">{errors.confirmPassword.message}</span>
+                )}
               </div>
               <Button type="submit" className="w-full hover:bg-blue-gray-300 rounded ">
                 Create an account
