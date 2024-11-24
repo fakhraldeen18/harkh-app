332587fcbe4ba39c83a39112c33981249e40a18d
HusamAlzain <husamalzain@hotmail.com>
2024-11-24 18:16:26 +0300

    Removed Dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,12 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from "./pages/Home/Home"
 import ProjectsPage from "./pages/Projects"
 import Index from "./pages/Projects/index"
+import Login from "./pages/Login/LogIn";
 function App() {
   return (
     <Router>
     <Routes>
-      {/* <Route path="/login" element={<Login />} /> */}
+       <Route path="/login" element={<Login />} /> 
       <Route path="/" element={<HomePage />} />
       <Route path="/projects" element={<ProjectsPage />} />
       <Route path="/index" element={<Index/>} />
